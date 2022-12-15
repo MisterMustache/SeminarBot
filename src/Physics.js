d1c92db34c70a036b15b26ca30503c1a2493cf19
duckduckwent <tadej.s02@gmail.com>
2022-12-15 22:35:04 +0100

    Basic movement and gltf model loading

---
+++ src/Physics.js
@@ -0,0 +1,110 @@
+import { vec3, mat4 } from '../lib/gl-matrix-module.js';
+
+export class Physics {
+
+    constructor(scene, controller) {
+        this.scene = scene;
+        this.controller = controller;
+    }
+
+    // checks if velocity is valid
+    checkVelocity(v) {
+        return v;
+    }
+
+    update(dt) {
+        this.controller.update(dt);
+
+        if (this.checkVelocity(this.controller.velocity)) {
+            // After moving, check for collision with every other node.
+            this.scene.traverse(node => {
+                if (node !== this.controller.node) {
+                    // this.resolveCollision(node, this.controller.node);
+                }
+            });
+        }
+    }
+
+    intervalIntersection(min1, max1, min2, max2) {
+        return !(min1 > max2 || min2 > max1);
+    }
+
+    aabbIntersection(aabb1, aabb2) {
+        return this.intervalIntersection(aabb1.min[0], aabb1.max[0], aabb2.min[0], aabb2.max[0])
+            && this.intervalIntersection(aabb1.min[1], aabb1.max[1], aabb2.min[1], aabb2.max[1])
+            && this.intervalIntersection(aabb1.min[2], aabb1.max[2], aabb2.min[2], aabb2.max[2]);
+    }
+
+    getTransformedAABB(node) {
+        // Transform all vertices of the AABB from local to global space.
+        const transform = node.globalMatrix;
+        const { min, max } = node.aabb;
+        const vertices = [
+            [min[0], min[1], min[2]],
+            [min[0], min[1], max[2]],
+            [min[0], max[1], min[2]],
+            [min[0], max[1], max[2]],
+            [max[0], min[1], min[2]],
+            [max[0], min[1], max[2]],
+            [max[0], max[1], min[2]],
+            [max[0], max[1], max[2]],
+        ].map(v => vec3.transformMat4(v, v, transform));
+
+        // Find new min and max by component.
+        const xs = vertices.map(v => v[0]);
+        const ys = vertices.map(v => v[1]);
+        const zs = vertices.map(v => v[2]);
+        const newmin = [Math.min(...xs), Math.min(...ys), Math.min(...zs)];
+        const newmax = [Math.max(...xs), Math.max(...ys), Math.max(...zs)];
+        return { min: newmin, max: newmax };
+    }
+
+    resolveCollision(a, b) {
+        console.log("resolveCollision");
+
+        // Get global space AABBs.
+        const aBox = this.getTransformedAABB(a);
+        const bBox = this.getTransformedAABB(b);
+
+        // Check if there is collision.
+        const isColliding = this.aabbIntersection(aBox, bBox);
+        if (!isColliding) {
+            return;
+        }
+
+        // Move node A minimally to avoid collision.
+        const diffa = vec3.sub(vec3.create(), bBox.max, aBox.min);
+        const diffb = vec3.sub(vec3.create(), aBox.max, bBox.min);
+
+        let minDiff = Infinity;
+        let minDirection = [0, 0, 0];
+        if (diffa[0] >= 0 && diffa[0] < minDiff) {
+            minDiff = diffa[0];
+            minDirection = [minDiff, 0, 0];
+        }
+        if (diffa[1] >= 0 && diffa[1] < minDiff) {
+            minDiff = diffa[1];
+            minDirection = [0, minDiff, 0];
+        }
+        if (diffa[2] >= 0 && diffa[2] < minDiff) {
+            minDiff = diffa[2];
+            minDirection = [0, 0, minDiff];
+        }
+        if (diffb[0] >= 0 && diffb[0] < minDiff) {
+            minDiff = diffb[0];
+            minDirection = [-minDiff, 0, 0];
+        }
+        if (diffb[1] >= 0 && diffb[1] < minDiff) {
+            minDiff = diffb[1];
+            minDirection = [0, -minDiff, 0];
+        }
+        if (diffb[2] >= 0 && diffb[2] < minDiff) {
+            minDiff = diffb[2];
+            minDirection = [0, 0, -minDiff];
+        }
+
+        vec3.add(a.translation, a.translation, minDirection);
+        a.updateTransformationMatrix();
+    }
+
+}
